2c5ecc1ded98f4af59886e67e3611d4949d65687
Sereyrotha chann <rathachann9900@gmail.com>
2020-07-10 07:53:26 +0700

    #3 add birth and picture to null

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('sex');
             $table->string('birth')->nullable();
             $table->string('picture')->nullable();
-            $table->BigInteger('role');
+            $table->BigInteger('role')->default(0);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
