47b53a2c1ed5a68d65b4423f10065559c4b258f1
lysasouy <lyza.souy@gmail.com>
2020-07-10 10:48:58 +0700

    #20 logout

--- resources/views/layouts/frontend/menuTamplate.blade.php
+++ resources/views/layouts/frontend/menuTamplate.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>dashborad</title>
+    <title>Letsgo</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
