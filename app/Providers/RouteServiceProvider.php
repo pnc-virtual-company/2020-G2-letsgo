a31bb836a0c7c1bc905eaae0f6d4f7b40a3cbacd
virak0001 <virakcambodia44@gmail.com>
2020-07-10 11:27:13 +0700

    change explore event

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/exploreEvent';
 
     /**
      * Define your route model bindings, pattern filters, etc.
