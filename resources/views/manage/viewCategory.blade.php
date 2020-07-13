5869fd83f9855b1976b1de5889d12998ae32a9da
sreyrot <sreyrot.oun@student.passerellesnumeriques.org>
2020-07-13 09:36:04 +0700

    update

--- resources/views/manage/viewCategory.blade.php
+++ resources/views/manage/viewCategory.blade.php
@@ -99,9 +99,10 @@
         @csrf
         <div class="modal-body">
           <input id="category" type="text" name="category" class="form-control" placeholder="Your category...." >
+       
           @error('category')
             <small class="text-danger">&nbsp;&nbsp;&nbsp;{{$message}}</small>           
-          @enderror
+          @enderror  
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-default" data-dismiss="modal">DISCARD</button>
