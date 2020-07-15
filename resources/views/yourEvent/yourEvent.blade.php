09a9093e264d07220fda43751181fee1b9f79b16
lysasouy <lyza.souy@gmail.com>
2020-07-15 14:17:05 +0700

    edit user

--- resources/views/yourEvent/yourEvent.blade.php
+++ resources/views/yourEvent/yourEvent.blade.php
@@ -2,4 +2,6 @@
 
 @section('body')
     <h3 class="text-center text-info mt-5">Views Of Your Events</h3>
+    <button type="button" class="btn btn-warning btn-sm text-white float-right" data-toggle="modal" data-target="#myModal"><span class="material-icons float-left">add</span><b>Create</b></button>
+
 @endsection
