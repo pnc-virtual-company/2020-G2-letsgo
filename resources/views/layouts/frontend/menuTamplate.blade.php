dd99224a3ccf1fb12ed87ea70d31a1d4bfc45292
Sereyrotha chann <rathachann9900@gmail.com>
2020-07-17 14:23:19 +0700

    update profile

--- resources/views/layouts/frontend/menuTamplate.blade.php
+++ resources/views/layouts/frontend/menuTamplate.blade.php
@@ -278,60 +278,7 @@
       </div>
     
     @yield('body')
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-     
-    <script type="text/javascript">
-      $('#file').on('change',function(ev){
-        console.log("here inside");
-        
-        var filedata=this.files[0];
-        var imgtype=filedata.type;
-     
-        var match=['image/png','image/jpg','image/jpeg','image/gif'];
-     
-        if(!((imgtype==match[0])||(imgtype==match[1])||(imgtype==match[2])||(imgtype==match[3])||(imgtype==match[4]))){
-            $('#mgs_ta').html('<p style="color:red">Plz select a valid type image..only png jpg jpeg gif allowed</p>');
-     
-        }else{
-     
-          $('#mgs_ta').empty();
-     
-        //---image preview
-     
-        var reader=new FileReader();
-     
-        reader.onload=function(ev){
-          $('#img_prv').attr('src',ev.target.result).css('width','120px').css('height','120px');
-        }
-        reader.readAsDataURL(this.files[0]);
-     
-        /// preview end
-
-            //upload
-     
-            var postData=new FormData();
-            postData.append('file',this.files[0]);
-     
-            var url="{{url('userImage.update')}}";
-     
-            $.ajax({
-            headers:{'X-CSRF-Token':$('meta[name=csrf_token]').attr('content')},
-            async:true,
-            type:"post",
-            contentType:false,
-            url:url,
-            data:postData,
-            processData:false,
-            success:function(){
-              console.log("success");
-            }
-     
-            });
-        }
-      });
-     
-    </script>
+    
 </body>
 </html>
 
