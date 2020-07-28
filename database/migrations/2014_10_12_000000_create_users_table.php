b4ee007ebde0d38cb7c16ed552532b1cd1224af9
Sereyrotha chann <rathachann9900@gmail.com>
2020-07-28 10:23:40 +0700

    fix some error

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -36,7 +36,7 @@ class CreateUsersTable extends Migration
                 'sex'=> 'Male',
                 'birth'=> null,
                 'picture'=> 'user.png',
-                'email' => 'virakcambodia44@gmail.com',
+                'email' => 'rathachann9900@gmail.com',
                 'role' =>1,
                 'password' => bcrypt('password'),
                 'remember_token' => Str::random(10)
