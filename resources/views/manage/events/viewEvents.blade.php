607bb011f5da363e1957849292465ec9f462373a
virak0001 <virakcambodia44@gmail.com>
2020-08-08 20:05:20 +0700

    fix bug: End date cannot before start date

--- resources/views/manage/events/viewEvents.blade.php
+++ resources/views/manage/events/viewEvents.blade.php
@@ -5,7 +5,7 @@
         {{-- Search form --}}
           {{-- <form action="" method="post"> --}}
             <div class="row">
-              <div class="input-icons col-md-8" style="margin: 0 auto"> 
+              <div class="input-icons col-md-9" style="margin: 0 auto"> 
                 <span class="material-icons">search</span> 
                 {{-- <input type="text" id="myInput"  onkeyup="myFunction()" placeholder="Search for names.." title="Type in a name"> --}}
                 <input class="form-control" id="search"  type="text" autocomplete="off" placeholder="Search"> 
@@ -16,39 +16,41 @@
           <br>
           <div class="form-group">
             <div class=" row">
-                <div class="col-md-8" style="margin:0 auto">
+                <div class="col-md-9" style="margin:0 auto">
                   <a class="h5">All Events</a>
                 </div>
               </div>
             </div>
           <div class="row">
-            <div class="col-md-8" style="margin:0 auto">
+            <div class="col-md-10" style="margin:0 auto">
               <div class="card">
-              <table class="table table-hover  table-borderless " id="myTable">
-                  <tr>
-                      <th>Organizer</th>
-                      <th>City</th>
-                      <th>Title</th>
-                      <th>Category</th>
-                      <th>Start date</th>
-
-                  </tr>
-                  <br>
-              
-                  @foreach ($event as $events)
-                    <tr class="event">
-                        <td>{{$events->user->firstname}}</td>
-                        <td>{{$events->city}}</td>
-                        <td>{{$events->title}}</td>
-                        <td>{{$events->category->category}}</td>
-                        <td>{{$events->startDate}}</td>
-                        <td >
-                        <a href="#!" class="deleteEvent" data-id="{{$events->id}}" data-toggle="modal" data-target="#deleteEvent"><i class="far fa-trash-alt text-dark" style="font-size:16px;"  id="deletes" ></i></a>
-                        </td>
+              <div class="table-responsive">
+                <table class="table table-hover  table-borderless " id="myTable">
+                    <tr>
+                        <th>Organizer</th>
+                        <th>City</th>
+                        <th>Title</th>
+                        <th>Category</th>
+                        <th>Start date</th>
+  
                     </tr>
-                    @endforeach
+                    <br>
                 
-              </table>
+                    @foreach ($event as $events)
+                      <tr class="event">
+                          <td>{{$events->user->firstname}}</td>
+                          <td>{{$events->city}}</td>
+                          <td>{{$events->title}}</td>
+                          <td>{{$events->category->category}}</td>
+                          <td>{{$events->startDate}}</td>
+                          <td >
+                          <a href="#!" class="deleteEvent" data-id="{{$events->id}}" data-toggle="modal" data-target="#deleteEvent"><i class="far fa-trash-alt text-dark" style="font-size:16px;"  id="deletes" ></i></a>
+                          </td>
+                      </tr>
+                      @endforeach
+                  
+                </table>
+              </div>
             </div>
             </div>
           </div>
