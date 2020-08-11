228cca47d26c8d979e8097ee8b7c703c46cd100d
virak0001 <virakcambodia44@gmail.com>
2020-08-11 08:22:48 +0700

    put icons of application

--- resources/views/layouts/frontend/menuTamplate.blade.php
+++ resources/views/layouts/frontend/menuTamplate.blade.php
@@ -2,24 +2,22 @@
 <html lang="en">
 <head>
     <title>Letsgo</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-
-
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link href="https://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.min.css" rel="stylesheet">
-  <script src="https://code.jquery.com/ui/1.10.2/jquery-ui.min.js"></script>
-  <link rel="stylesheet" href="{{asset('asset/css/main.css')}}">
-  <link rel="stylesheet" href="{{asset('asset/css/style.css')}}">
-  <script src="{{ asset('asset/js/awaresome.js') }}"></script>
-  <script src="{{ asset('asset/js/readCityList.js') }}"></script>
-  <script src="{{ asset('asset/js/main.js') }}"></script>
+    <link rel="shortcut icon" href="{{asset('asset/logo/logo.png')}}" type="image/x-icon">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/ui/1.10.2/jquery-ui.min.js"></script>
+    <link rel="stylesheet" href="{{asset('asset/css/main.css')}}">
+    <link rel="stylesheet" href="{{asset('asset/css/style.css')}}">
+    <script src="{{ asset('asset/js/awaresome.js') }}"></script>
+    <script src="{{ asset('asset/js/readCityList.js') }}"></script>
+    <script src="{{ asset('asset/js/main.js') }}"></script>
 </head>
 <body>
        <nav class="navbar p-0 navbar-expand-md navbar-light bg-white shadow-sm h5 fixed-top">
